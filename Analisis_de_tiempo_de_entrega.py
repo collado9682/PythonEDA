0f7802601c11488346331b1327ca792f2579ecab
collado9682 <genesiscollado813@gmail.com>
2025-04-13 20:59:13 -0600

    Importando las librerias

---
+++ Analisis_de_tiempo_de_entrega.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+file_path="./Data/Pen Sales Data.xlsx" #archivo excel
+
+#ALMACENAMOS EL ARCHIVO EN UN DATAFRAME
+df_pen_sales= pd.read_excel(file_path,sheet_name="Pen Sales")
+
+
