8c6897c6eaf007c708a5fbe17b5ffe0d0edd4a15
collado9682 <genesiscollado813@gmail.com>
2025-04-13 19:00:06 -0600

    Creando el gráfico para representar los costos de envío

--- Analisis_de_costos_de_envio.py
+++ Analisis_de_costos_de_envio.py
@@ -16,11 +16,20 @@ df_pen_sales= pd.read_excel(file_path,sheet_name="Pen Sales")
 #Visualización: 📊 Gráfico de barras (costo promedio de envío por artículo)
 
 #Calcular la distribución del Costo de Envío.
-df_distribución_costo_envío= df_pen_sales["Shipping Cost"].describe()
+df_distribucion_costo_envío= df_pen_sales["Shipping Cost"].describe()
 
 #Agrupe por artículo y calcule el costo promedio de envío.
 df_avg_pen_costs= df_pen_sales.groupby("Item")["Shipping Cost"].mean().sort_values()
 
-
 #print(df_distribución_costo_envío)
 print(df_avg_pen_costs)
+
+
+#Cree un gráfico de barras que compare los costos de envío por tipo de bolígrafo.
+plt.figure(figsize=(10, 5))
+df_avg_pen_costs.plot(kind="barh", color="purple")
+plt.title("Costo de envío promedio por producto")
+plt.xlabel("Costo medio de envío")
+plt.ylabel("Tipo de Producto")
+plt.tight_layout()
+plt.show()
