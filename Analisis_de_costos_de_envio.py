47e625884932839ff644b19c32f949963df34db3
collado9682 <genesiscollado813@gmail.com>
2025-04-13 18:56:37 -0600

    Completando las subtareas 1 y 2 en relación con el análisis de costos de envíos

--- Analisis_de_costos_de_envio.py
+++ Analisis_de_costos_de_envio.py
@@ -5,3 +5,22 @@ file_path="./Data/Pen Sales Data.xlsx" #archivo excel
 
 #ALMACENAMOS EL ARCHIVO EN UN DATAFRAME
 df_pen_sales= pd.read_excel(file_path,sheet_name="Pen Sales")
+
+#INCISO 2
+#Análisis de costos de envío
+#Tarea: Evalúe cómo varían los costos de envío entre pedidos.
+#Pasos:
+#Calcular la distribución del Costo de Envío.
+#Agrupe por artículo y calcule el costo promedio de envío.
+#Cree un gráfico de barras que compare los costos de envío por tipo de bolígrafo.
+#Visualización: 📊 Gráfico de barras (costo promedio de envío por artículo)
+
+#Calcular la distribución del Costo de Envío.
+df_distribución_costo_envío= df_pen_sales["Shipping Cost"].describe()
+
+#Agrupe por artículo y calcule el costo promedio de envío.
+df_avg_pen_costs= df_pen_sales.groupby("Item")["Shipping Cost"].mean().sort_values()
+
+
+#print(df_distribución_costo_envío)
+print(df_avg_pen_costs)
