bff359d2644e0237d7ab8cc65174f104b4326c22
collado9682 <genesiscollado813@gmail.com>
2025-04-13 20:22:17 -0600

    Añadiendo la subtarea con respecto al ranking de popularidad de boligrafos

--- Ranking_de_popularidad_de_boligrafos.py
+++ Ranking_de_popularidad_de_boligrafos.py
@@ -5,3 +5,17 @@ file_path="./Data/Pen Sales Data.xlsx" #archivo excel
 
 #ALMACENAMOS EL ARCHIVO EN UN DATAFRAME
 df_pen_sales= pd.read_excel(file_path,sheet_name="Pen Sales")
+
+#INCISO 3
+#Ranking de popularidad de bolígrafos
+#Tarea: Identificar el tipo de bolígrafo que se compra con más frecuencia.
+#Pasos:
+#Cuente el número de compras por artículo.
+#Ordenar en orden descendente.
+#Traza un gráfico de barras horizontales para mayor claridad.
+#Visualización: 📊 Gráfico de barras horizontales (bolígrafos más vendidos)
+
+#Cuente el número de compras por artículo.
+Conteo_de_productos= df_pen_sales ["Item"].value_counts()
+
+print(Conteo_de_productos)
