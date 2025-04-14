b4af2ed1935eedf1e905d3565fca8744d23086a8
collado9682 <genesiscollado813@gmail.com>
2025-04-13 21:08:53 -0600

    Completando las subtareas 1 y 2

--- Analisis_de_tiempo_de_entrega.py
+++ Analisis_de_tiempo_de_entrega.py
@@ -6,4 +6,25 @@ file_path="./Data/Pen Sales Data.xlsx" #archivo excel
 #ALMACENAMOS EL ARCHIVO EN UN DATAFRAME
 df_pen_sales= pd.read_excel(file_path,sheet_name="Pen Sales")
 
+#INCISO 4
+#Análisis de Tiempo de Entrega
+#Tarea: Calcular el tiempo medio de entrega para cada tipo de bolígrafo.
+#Pasos:
+#Calcular tiempo de entrega = Fecha de entrega - Fecha de compra.
+#Agrupe por artículo y encuentre el tiempo medio de entrega.
+#Traza un gráfico de barras para comparar los tiempos de entrega.
+#Visualización: ⏳ Gráfico de barras (tiempo medio de entrega por tipo de bolígrafo)
 
+#Calcular tiempo de entrega = Fecha de entrega - Fecha de compra.
+Tiempo_de_entrega= (df_pen_sales["Delivery Date"] - df_pen_sales["Purchase Date"])
+print(Tiempo_de_entrega)
+
+#Columna nueva
+df_pen_sales["Tiempo de entrega"] = Tiempo_de_entrega
+
+#print (df_pen_sales.dtypes)
+
+#Agrupe por artículo y encuentre el tiempo medio de entrega.
+Tiempo_medio_entrega = df_pen_sales.groupby("Item") ["Tiempo de entrega"].mean().sort_values()
+
+print(Tiempo_medio_entrega)
